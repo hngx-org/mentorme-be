7f8a988f08ac2b626a8c2d425c2dadb27c16113b
Ifechukwu Ogidi <ogidiifechukwu6@gmail.com>
2023-10-19 13:08:34 +0100

    Feat: Created Views for Session and Categories

--- mentoring/views.py
+++ mentoring/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
-from .models import Session
-from .serializers import SessionSerializer
+from .models import Session, Category, Mentor
+from .serializers import SessionSerializer, CategorySerializer
 
 
 # Create your views here.
@@ -13,6 +14,27 @@ class SessionCreateAPIView(generics.CreateAPIView):
 
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
+        try:
+            mentor = Mentor.objects.get(user=request.user)
+        except Mentor.DoesNotExist:
+            return Response({"detail": "Not a mentor"},
+                            status=403)
+        serializer["mentor"] = mentor
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=201)
+
+
+class CategoryListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=201)
+
+    def get(self, request, *args, **kwargs):
+        serializer = self.serializer_class(self.get_queryset(), many=True)
+        return Response(serializer.data)
