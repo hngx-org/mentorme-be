3c680814fb8042a8aae6180d3fd7b3dbc0da8166
Yuguda999 <95680262+Yuguda999@users.noreply.github.com>
2023-10-19 09:43:35 +0100

    add get sessions for a mentor

--- mentoring/views.py
+++ mentoring/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.response import Response
+from .models import Session
+from .serializers import SessionSerializer
 
-# Create your views here.
+class MentorSessionList(generics.ListAPIView):
+    serializer_class = SessionSerializer
+
+    def get_queryset(self):
+        mentor_id = self.kwargs.get('mentor_id')
+        try:
+            sessions = Session.objects.filter(mentor__user__id=mentor_id)
+            return sessions
+        except Session.DoesNotExist:
+            return Response(
+                {"error": "No sessions found for the specified mentor."},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        except Exception as e:
+            return Response(
+                {"error": str(e)},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+    
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
