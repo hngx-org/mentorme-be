21faac73c8463d5c8a55cd4b8ce6177811e4f60a
Beloved-vince <vinceoludare@gmail.com>
2023-10-19 15:54:33 +0100

    2 error left

--- mentoring/serializers.py
+++ mentoring/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 import re
 from .models import *
-from .models import Session
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
@@ -25,54 +24,95 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
         return value
     
-from rest_framework import serializers
-class MentorProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Mentor
-        fields = '__all__'
-
-    def create(self, validated_data):
-        request = self.context.get('request')
-        if request and hasattr(request, 'user') and isinstance(request.user, CustomUser):
-            user_id = request.user.id
-
-            education_data = validated_data.pop('education')
-            company_data = validated_data.pop('company')
-            industry_data = validated_data.pop('industry')
-            skill_data = validated_data.pop('skill')
-            session_data = validated_data.pop('session')
-            identity_data = validated_data.pop('identity')
-            certification_data = validated_data.pop('certification')
-            resource_data = validated_data.pop('resource')
-
-            education, created = Education.objects.get_or_create(**education_data)
-            company_t, created = Company.objects.get_or_create(**company_data)
-            industry, created = Industry.objects.get_or_create(**industry_data)
-            skill, created = Skill.objects.get_or_create(**skill_data)
-            session, created = Session.objects.get_or_create(**session_data)
-            identity, created = Identity.objects.get_or_create(**identity_data)
-            certification, created = Certification.objects.get_or_create(**certification_data)
-            resource, created = Resource.objects.get_or_create(**resource_data)
-
-            mentor = Mentor.objects.create(
-                user_id=user_id,
-                education=education.id,
-                company_=company_t.id,
-                industry=industry.id,
-                skill=skill.id,
-                session=session.id,
-                identity=identity.id,
-                certification=certification.id,
-                resource=resource.id,
-                **validated_data
-            )
-
-            return mentor
-        else:
-            raise serializers.ValidationError("Only authenticated CustomUser can create a Mentor.")
-
 
 class SessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Session
         fields = '__all__'
+
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class ResourceSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()  # Nested serialization of Category
+    class Meta:
+        model = Resource
+        fields = '__all__'
+
+
+class EducationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Education
+        fields = '__all__'
+
+class IndustrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Industry
+        fields = '__all__'
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = '__all__'
+
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = '__all__'
+
+class CertificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Certification
+        fields = '__all__'
+
+class IdentitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Identity
+        fields = '__all__'
+
+class MentorSerializer(serializers.ModelSerializer):
+    user = CustomUserSerializer()
+    company = CompanySerializer() 
+    industry = IndustrySerializer()  
+    skills = SkillSerializer()  
+    education = EducationSerializer()
+    certification = CertificationSerializer()
+    identity = IdentitySerializer() 
+    resources = ResourceSerializer()
+    class Meta:
+        model = Mentor 
+        fields = '__all__'
+        
+    def create(self, validated_data):
+        request = self.context.get('request')
+        if request and hasattr(request, 'user'):
+            user = request.user
+
+            validated_data['user'] = user.id
+        
+        company_id = validated_data.pop('company')
+        industry_id = validated_data.pop('industry')
+        skills_id = validated_data.pop('skills')
+        education_id = validated_data.pop('education')
+        certification_id = validated_data.pop('certification')
+        identity_id = validated_data.pop('identity')
+        resources_id = validated_data.pop('resources')
+        sessions_id = validated_data.pop('sessions')
+
+        mentor = Mentor.objects.create(**validated_data)
+
+        mentor.user_id = user
+        mentor.company_id = company_id
+        mentor.industry_id = industry_id
+        mentor.skills_id = skills_id
+        mentor.education_id = education_id
+        mentor.certification_id = certification_id
+        mentor.identity_id = identity_id
+        mentor.resources_id = resources_id
+        mentor.sessions_id = sessions_id
+
+        return mentor
--- mentoring/views.py
+++ mentoring/views.py
@@ -2,7 +2,6 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.views.decorators.csrf import csrf_exempt
-from .serializers import MentorProfileSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 from rest_framework import status
@@ -11,8 +10,9 @@ from .models import *
 from .serializers import *
 
 class MentorCreationView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Mentor.objects.all()
-    serializer_class = MentorProfileSerializer
+    serializer_class = MentorSerializer
 
     def perform_create(self, serializer):
         user = self.request.user  
